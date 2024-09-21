88c14a6465009c0c877e9e09f5b44d65f52cc728
DK <ashukr8081@gmail.com>
2024-09-21 11:29:01 +0530

    add some files

---
+++ DSA/C21Bipartite.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class C21Bipartite { // Bipartite graph : A graph whose vertices can be divided into two independent sets, U and V.
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        // graph[3].add(new Edge(3, 4));
+
+        graph[4].add(new Edge(4, 2));
+        // graph[4].add(new Edge(4, 3));
+
+    }
+
+    public static boolean isPartite(ArrayList<Edge>[] graph) { //O(V+E)
+        int color[] = new int[graph.length];
+
+        for (int i = 0; i < color.length; i++) {
+            color[i] = -1;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < graph.length; i++) {
+            if (color[i] == -1) {// BFS
+                q.add(i);
+                color[i] = 0; // Yellow
+                while (!q.isEmpty()) {
+                    int curr = q.remove();
+                    for (int j = 0; j < graph[curr].size(); j++) {
+                        Edge e = graph[curr].get(j); // e.dest
+                        if (color[e.dest] == -1) {
+                            int nextCol = color[curr] == 0 ? 1 : 0;
+                            color[e.dest] = nextCol;
+                            q.add(e.dest);
+                        } else if (color[e.dest] == color[curr]) {
+                            return false; // NOT
+
+                        }
+                    }
+
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        System.out.println(isPartite(graph));
+    }
+}
---
+++ DSA/C21CycleDetectionInDirGraph.java
@@ -0,0 +1,5 @@
+public class C21CycleDetectionInDirGraph {// Cycle detection in directed graph
+    public static void main(String[] args) {
+        
+    }
+}
