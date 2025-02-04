a1dfb9a2f801c02c01ed2dc18facfbae1404a0aa
DK <ashukr8081@gmail.com>
2025-02-04 23:25:40 +0530

    feat: prompt user for radius input in circle area calculation

--- Basic Java/c1arOfcircle.java
+++ Basic Java/c1arOfcircle.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class C1arOfcircle {
     public static void main(String[] args) {
+        System.out.println("Enter radius:");
         Scanner sc = new Scanner(System.in);
         int radius = sc.nextInt();
         float area = (float) 3.14*radius*radius;
