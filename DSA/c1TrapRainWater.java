633843b0d1300169bfaeeefc4ac8b76a56e7247b
DK <ashukr8081@gmail.com>
2025-02-05 00:12:32 +0530

    fix: update comments for clarity in C1TrapRainWater class

--- DSA/c1TrapRainWater.java
+++ DSA/c1TrapRainWater.java
@@ -1,14 +1,14 @@
 public class C1TrapRainWater {
 
     public static int trappedRainWater(int height[]) {
-        // calculate left max boundary - array
+        // calculate left max boundary -> array
         int n = height.length;
         int leftMax[] = new int[n];
         leftMax[0] = height[0];
         for (int i = 1; i < n; i++) {
             leftMax[i] = Math.max(height[i], leftMax[i - 1]);
         }
-        // calculate right max boundary - array
+        // calculate right max boundary -> array
         int rightMax[] = new int[n];
         rightMax[n - 1] = height[n - 1];
         for (int i = n - 2; i >= 0; i--) {
